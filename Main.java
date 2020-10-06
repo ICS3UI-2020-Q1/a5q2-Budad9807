4290bdd5479e354ff5d3319fd3e0f1068f9d1078
Budad9807 <Budad9807@wrdsb.ca>
2020-10-06 15:50:28 +0000

    Made a program the prints one through 7 with commas

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Typing a postive interger prints out numbers in space with comma
+ * @author David
  */
 public class Main {
 
@@ -9,7 +10,15 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create the count varibale
+    int count = 1;
+    // create a loop that goes up to 7 with commas
+    while(count <= 7){
+      // loop action print the number to the console
+      System.out.print(count + ",");
+      // increase the count
+      count = count + 1;
+    }
+    // it print out 1 through 7 with commas
   }
 }
